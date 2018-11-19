a8593b89247d6d08de2a0063fc48654331df6336
Romain Lesur <romain.lesur@gmail.com>
2018-11-19 03:57:06 +0100

    add pipes

---
+++ R/utils-pipe.R
@@ -0,0 +1,51 @@
+#' Pipe operators
+#'
+#' See \code{promises::\link[promises]{pipes}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom promises %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' @name %T>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom promises %T>%
+#' @usage lhs \%T>\% rhs
+NULL
+
+#' @name %...>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom promises %...>%
+#' @usage lhs \%...>\% rhs
+NULL
+
+#' @name %...T>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom promises %...T>%
+#' @usage lhs \%...T>\% rhs
+NULL
+
+#' @name %...!%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom promises %...!%
+#' @usage lhs \%...!\% rhs
+NULL
+
+#' @name %...T!%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom promises %...T!%
+#' @usage lhs \%...T!\% rhs
+NULL
