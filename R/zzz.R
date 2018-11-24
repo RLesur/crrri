e61c74e6c3d4765d085db43569e6caa0ec33797c
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-11-24 17:59:32 +0100

    Use debugme (#13)
    * add debugme requirement
    
    * add a part about debugme in readme
    
    * add a message as debugme is a Suggested dependency

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onLoad <- function(libname, pkgname) {
+  if(requireNamespace("debugme", quietly = TRUE)) {
+    debugme::debugme()
+  } else {
+    if ("crrri" %in% strsplit(Sys.getenv("DEBUGME"), split = ",")[[1]]) {
+      message("It seems you have this package in your DEBUGME variable,",
+              "but you have not install the debugme package yet.\n",
+              "You need to install it to the the log messages.")
+    }
+  }
+}
