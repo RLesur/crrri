96ef5c9d0fc686b6b00c91d25debe6fcc670b3e2
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-12-23 20:19:43 +0100

    first try based on @RLesur draft in #8

---
+++ R/eventemitters.R
@@ -0,0 +1,122 @@
+#' R6 class to emit some event
+#'
+#' This is a general purpose class to build on. It is inspired by node.js EventEmitter class.
+#' See https://nodejs.org/api/events.html
+#'
+#' @name EventEmitter
+#' @example
+#' \dontrun{
+#' myEmitter <- EventEmitter$new()
+#' myEmitter$on('event',
+#'     function() {
+#'         message("an event occured")
+#'     }
+#' )
+#' myEmitter$emit('event')
+#' # Listener is still listening
+#' myEmitter$emit('event')
+#' myEmitter$once('uniqueevent',
+#'     function() {
+#'         message("an event occured - only once")
+#'     }
+#' )
+#' # the listerner will be removed after first emission of the event
+#' myEmitter$emit('uniqueevent')
+#' # nothing happens as there is no more listener
+#' myEmitter$emit('uniqueevent')
+#' }
+NULL
+
+
+#' @rdname EventEmitter
+#' @export
+EventEmitter <- R6::R6Class(
+  "EventEmitter",
+  private = list(
+    callbacks = list()
+  ),
+  public = list(
+    emit = function(eventName, ...) {
+      callbacks <- private$callbacks[[eventName]]
+      if (eventName == "error" && length(callbacks) == 0) {
+        stop(...)
+      }
+      if (length(callbacks) > 0) {
+        tryCatch(callbacks$invoke(...),
+          error = function(e) self$emit("error", e)
+        )
+      } else {
+        invisible(NULL)
+      }
+    },
+    on = function(eventName, callback) {
+      callbacks <- private$callbacks[[eventName]]
+      if (length(callbacks) == 0) {
+        private$callbacks[[eventName]] <- Callbacks$new()
+      }
+      self$emit("newListener", eventName, callback)
+      private$callbacks[[eventName]]$register(callback)
+      invisible(self)
+    },
+    once = function(eventName, callback) {
+      callbacks <- private$callbacks[[eventName]]
+      if (length(callbacks) == 0) {
+        private$callbacks[[eventName]] <- Callbacks$new()
+      }
+      remove_callback <- NULL
+      new_callback <- function(...) {
+        callback(...)
+        remove_callback()
+        # TODO: See if required or not
+        self$emit("removeListener", eventName, callback)
+      }
+      remove_callback <- private$callbacks[[eventName]]$register(new_callback)
+    },
+    listenerCount = function(eventName) {
+      length(private$callbacks[[eventName]])
+    }
+  )
+)
+
+# from rstudio/websocket
+# https://github.com/rstudio/websocket/blame/master/R/websocket.R
+Callbacks <- R6::R6Class(
+  "Callbacks",
+  private = list(
+    .nextId = integer(0),
+    .callbacks = "environment"
+  ),
+  public = list(
+    initialize = function() {
+      # NOTE: we avoid using '.Machine$integer.max' directly
+      # as R 3.3.0's 'radixsort' could segfault when sorting
+      # an integer vector containing this value
+      private$.nextId <- as.integer(.Machine$integer.max - 1L)
+      private$.callbacks <- new.env(parent = emptyenv())
+    },
+    register = function(callback) {
+      if (!is.function(callback)) {
+        stop("callback must be a function")
+      }
+      id <- as.character(private$.nextId)
+      private$.nextId <- private$.nextId - 1L
+      private$.callbacks[[id]] <- callback
+      return(function() {
+        rm(list = id, pos = private$.callbacks)
+      })
+    },
+    invoke = function(...) {
+      # Ensure that calls are invoked in the order that they were registered
+      keys <- as.character(sort(as.integer(ls(private$.callbacks)), decreasing = TRUE))
+      callbacks <- mget(keys, private$.callbacks)
+
+      for (callback in callbacks) {
+        res <- callback(...)
+        cat(res)
+      }
+    },
+    count = function() {
+      length(ls(private$.callbacks))
+    }
+  )
+)
