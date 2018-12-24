d6570aef4eca534c27dd16ea1b59c993f950d320
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-12-24 11:37:42 +0100

    update clean function

--- tools/clean.R
+++ tools/clean.R
@@ -1,6 +1,6 @@
 
 clean_generated_R <- function(dry_run = FALSE) {
-  generated_files <- list.files(here::here("R"), pattern = "(events|commands)\\.R$", full.names = TRUE)
+  generated_files <- list.files(here::here("R"), pattern = "^(events|commands)_", full.names = TRUE)
   if (dry_run) {
     generated_files
   } else {
