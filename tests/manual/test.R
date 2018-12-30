488dd2f2a727cf011ac64c1b36c1e6467a69d065
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-12-30 19:11:59 +0100

    add a manual test file as test and example while developping.
    Aim: keep trace of in dev testing

---
+++ tests/manual/test.R
@@ -0,0 +1,48 @@
+devtools::load_all()
+
+work_dir <- chr_new_data_dir()
+
+chrome <- chr_launch(work_dir = work_dir, headless = TRUE)
+
+ws_endpoint <- chr_get_ws_addr(debug_port = 9222)
+
+page_session <- CDPSession$new(ws_endpoint)
+
+page_session$.__enclos_env__$private$.CDPSession_con$readyState()
+page_session$.__enclos_env__$private$.CDPSession_con$connect()
+
+# listening event
+page_session$on("Runtime.executionContextCreated", function(...) cat("First command passed!"))
+
+page_session$on("1",
+                ~ page_session$sendCommand('Page.enable'))
+page_session$on("2",
+                ~ page_session$sendCommand('Runtime.addBinding', params = list(name = "pagedownListener")))
+page_session$on("3",
+                ~ page_session$sendCommand('Page.navigate',
+                                         params = list(url = "file:///C:/Users/chris/Documents/test.nb.html")))
+page_session$on('Page.domContentEventFired',
+                ~ page_session$sendCommand('Runtime.evaluate',
+                                           params = list(expression = "!!window.PagedPolyfill")
+                ))
+page_session$on("5",
+                function(data) if (!isTRUE(data$result$result$value)) {
+                    page_session$sendCommand("Page.printToPDF",
+                                    params = list(printBackground = TRUE, preferCSSPageSize = TRUE))
+})
+page_session$on('Runtime.bindingCalled',
+                ~ page_session$send("Page.printToPDF",
+                                    params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
+                ))
+page_session$on("6",
+               function(data) writeBin(jsonlite::base64_dec(data$result$data), "test.pdf"))
+
+
+page_session$sendCommand('Runtime.enable')
+
+
+
+
+page_session$.__enclos_env__$private$.CDPSession_con$close()
+if(chrome$is_alive()) chrome$kill()
+rm(list = ls())
