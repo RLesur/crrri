82db80c1c48528c689bf4723070628cd337c31f3
Romain Lesur <romain.lesur@gmail.com>
2019-04-12 08:48:41 +0200

    update EventsEmitter

--- tests/manual/manual_test_eventemitters.R
+++ tests/manual/manual_test_eventemitters.R
@@ -1,55 +1,62 @@
 myEmitter <- EventEmitter$new()
-myEmitter$on('event',
-    function() {
-        message("an event occured")
-    }
+myEmitter$on("event",
+             function() {
+               message("an event occured!")
+             }
 )
-myEmitter$emit('event')
-# Listener is still listening
-myEmitter$emit('event')
-myEmitter$once('uniqueevent',
-    function() {
-        message("an event occured - only once")
-    }
+myEmitter$emit("event")
+# The listener is still called when "event" fires:
+myEmitter$emit("event")
+# A listener can be registered once:
+myEmitter$once("event",
+               function() {
+                 message("this listener is called only once")
+               }
 )
-# the listerner will be removed after first emission of the event
-myEmitter$emit('uniqueevent')
-# nothing happens as there is no more listener
-myEmitter$emit('uniqueevent')
+# This new listener will be removed when called.
+myEmitter$emit("event")
+# There is now only one listener:
+myEmitter$emit("event")
 
-# an error in one listener will throws an error in R
-myEmitter$on("eventwitherror", function(...) stop("An error in an event"))
-# throws an error
-myEmitter$emit("eventwitherror")
-# you can catch an error with a special "error" event that always
-# be emits when an error occured inside an eventEmitters
+# An error in a listener will throw an error in R.
+myEmitter$on("event", function(...) stop("An error in a listener"))
+# Throw an error:
+## Not run:
+myEmitter$emit("event")
+## End(Not run)
+
+# You can catch an error with a special "error" event that is
+# always emitted when an error occured in a listener:
 myEmitter$on("error", function(e) cat(conditionMessage(e)))
-myEmitter$emit("eventwitherror")
+myEmitter$emit("event")
 
-# As in node.js class, a newListener event is emitted before each new event registration.
-# newListener expect a function with eventName and callback argument
+# As in the node.js class, a "newListener" event is emitted
+# before each new listener registration.
+# The "newListener" event passes two arguments to its listener(s):
+# `eventName` and `listener`.
 myEmitter <- EventEmitter$new()
-# use once here to avoir infinite recursion
-myEmitter$once("newListener", function(eventName, callback) {
-   if(eventName == "event") {
-      myEmitter$on("event", function(...) cat("B"))
-   }
+# Use once here to avoid infinite recursion:
+myEmitter$once("newListener", function(eventName, listener) {
+  if(eventName == "event") {
+    myEmitter$on("event", function(...) cat("B"))
+  }
 })
 myEmitter$on("event", function(...) cat("A"))
 myEmitter$emit("event") # BA
-# newListener has been unregistered and is no more called
+# The listener attached to the "newListener" event has been
+# unregistered and is no more called:
 myEmitter$on("event", function(...) cat("C"))
 myEmitter$emit("event") # BAC
 
-# get the number of listeners for an event
-myEmitter$listenerCount('event')
+# Get the number of listeners for an event:
+myEmitter$listenerCount("event")
 
-# get the event names which have been registered
+# Get the event names which have been registered:
 myEmitter$eventNames()
 
 
 # Once is use for any listener that should be activated only once
 myEmitter <- EventEmitter$new()
-myEmitter$once("a.listener", ~ cat(.x))
+myEmitter$once("a.listener", rlang::as_function(~ cat(.x)))
 myEmitter$emit("a.listener", "A")
 myEmitter$emit("a.listener", "B")
