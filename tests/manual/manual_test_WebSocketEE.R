7b6376c5e6d49282aa1b347be6d49c2eb1fb1d6b
Romain Lesur <romain.lesur@gmail.com>
2019-04-14 02:24:56 +0200

    turn websocket in an EventEmitter as in JS

---
+++ tests/manual/manual_test_WebSocketEE.R
@@ -0,0 +1,19 @@
+devtools::load_all()
+
+work_dir <- chr_new_data_dir()
+
+chrome <- chr_launch(work_dir = work_dir, headless = TRUE)
+
+ws_endpoint <- chr_get_ws_addr(debug_port = 9222)
+
+ws <- WebSocketEE$new(ws_endpoint, onconnect = function(client) {cat("Connected!\n")})
+ws$connect()
+ws$on("event", function(data) {
+  cat("Message from Chrome", data, "\n")
+})
+ws$emit("message", "test")
+
+ws$close()
+if(chrome$is_alive()) chrome$kill()
+rm(list = ls())
+gc()
