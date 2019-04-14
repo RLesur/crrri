a948fd78f02981681f5406bfc203913728fab050
Romain Lesur <romain.lesur@gmail.com>
2019-04-14 18:39:15 +0200

    chain once

--- tests/manual/manual_test_CDPSession.R
+++ tests/manual/manual_test_CDPSession.R
@@ -13,34 +13,41 @@ page_session$.__enclos_env__$private$.CDPSession_con$readyState()
 # listening event
 page_session$once("Runtime.executionContextCreated", function(...) cat("First command passed!"))
 
-page_session$once("connect", ~ page_session$sendCommand('Runtime.enable'))
-
-page_session$once("Runtime.enable",
-                ~ page_session$sendCommand('Page.enable'))
-page_session$once("Page.enable",
-                ~ page_session$sendCommand('Runtime.addBinding', params = list(name = "pagedownListener")))
-page_session$once("Runtime.addBinding",
-                ~ page_session$sendCommand('Page.navigate',
-                                         params = list(url = "https://pagedown.rbind.io")))
-page_session$once('Page.domContentEventFired',
-                ~ page_session$sendCommand('Runtime.evaluate',
-                                           params = list(expression = "!!window.PagedPolyfill")
-                ))
-page_session$once(
-  "Runtime.evaluate",
-  function(result) if (!isTRUE(result$result$value)) {
-    page_session$sendCommand(
-      "Page.printToPDF",
-      params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
-    )
-  }
-)
-page_session$once('Runtime.bindingCalled',
-                ~ page_session$sendCommand("Page.printToPDF",
-                                    params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
-                ))
-page_session$once("Page.printToPDF",
-               function(result) writeBin(jsonlite::base64_dec(result$data), "test.pdf"))
+page_session$
+  once("connect", ~ page_session$sendCommand('Runtime.enable'))$
+  once("Runtime.enable", ~ page_session$sendCommand('Page.enable'))$
+  once("Page.enable",
+       ~ page_session$sendCommand('Runtime.addBinding',
+                                  params = list(name = "pagedownListener")
+       )
+  )$
+  once("Runtime.addBinding",
+       ~ page_session$sendCommand('Page.navigate',
+                                  params = list(url = "https://pagedown.rbind.io")
+       )
+  )$
+  once('Page.domContentEventFired',
+       ~ page_session$sendCommand('Runtime.evaluate',
+                                  params = list(expression = "!!window.PagedPolyfill")
+       )
+  )$
+  once("Runtime.evaluate",
+       function(result) if (!isTRUE(result$result$value)) {
+         page_session$sendCommand(
+           "Page.printToPDF",
+          params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
+         )
+       }
+  )$
+  once('Runtime.bindingCalled',
+       ~ page_session$sendCommand(
+         "Page.printToPDF",
+         params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
+       )
+  )$
+  once("Page.printToPDF",
+       function(result) writeBin(jsonlite::base64_dec(result$data), "test.pdf")
+  )
 
 # connect
 page_session$connect()
