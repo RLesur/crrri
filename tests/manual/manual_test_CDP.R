427c817ad43f836d621f5959cc5e826b8d1c4c8b
Romain Lesur <romain.lesur@gmail.com>
2019-04-19 01:06:52 +0200

    update manual tests

--- tests/manual/manual_test_CDP.R
+++ tests/manual/manual_test_CDP.R
@@ -1,25 +1,102 @@
-# CDP ---------------------------------------------------------------------
 devtools::load_all()
 
-work_dir <- chr_new_data_dir()
-
-chrome <- chr_launch(work_dir = work_dir, headless = TRUE)
-
-ws_endpoint <- chr_get_ws_addr(debug_port = 9222)
-
-client <- CDP(ws_endpoint)
-
-client$on("connect", function(x) cat("coucou\n"))
-
-client$connect()
+remote <- Chrome$new()
+client <- CDP()
+# client is in pre-connecting test until connection is explicitely launch
 client$readyState()
 
+# listening event
+client$once("Runtime.executionContextCreated", function(...) cat("First command passed!"))
 
-a = client$send("Runtime.enable")
+client$
+  once("connect", ~ client$send('Runtime.enable'))$
+  once("Runtime.enable", ~ client$send('Page.enable'))$
+  once("Page.enable",
+       ~ client$send('Runtime.addBinding',
+                     params = list(name = "pagedownListener")
+       )
+  )$
+  once("Runtime.addBinding",
+       ~ client$send('Page.navigate',
+                     params = list(url = "https://pagedown.rbind.io")
+       )
+  )$
+  once('Page.domContentEventFired',
+       ~ client$send('Runtime.evaluate',
+                     params = list(expression = "!!window.PagedPolyfill")
+       )
+  )$
+  once("Runtime.evaluate",
+       function(result) if (!isTRUE(result$result$value)) {
+         client$send(
+           "Page.printToPDF",
+           params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
+         )
+       }
+  )$
+  once('Runtime.bindingCalled',
+       ~ client$send(
+         "Page.printToPDF",
+         params = list(printBackground = TRUE, preferCSSPageSize = TRUE)
+       )
+  )$
+  once("Page.printToPDF",
+       function(result) writeBin(jsonlite::base64_dec(result$data), "test.pdf")
+  )
 
-b = client$send("rr")
+# Lauching connection and starting the chain of event
+client$connect()
 
-client$close()
-if(chrome$is_alive()) chrome$kill()
+# disconnect the client and close chrome
+client$disconnect()
+if(remote$is_alive) remote$close()
+rm(list = ls())
+gc()
+
+# New API with promises --------------------------------------
+devtools::load_all()
+
+remote <- Chrome$new()
+client <- remote$connect()
+
+# client is connected and ready to receive commands
+client$readyState()
+
+Runtime <- client$Runtime
+Page <- client$Page
+
+# listening event
+Runtime$executionContextCreated(function(...) cat("First command passed!"))
+
+print_pdf <- function() {
+  Page$printToPDF(
+    printBackground = TRUE,
+    preferCSSPageSize = TRUE,
+    callback = function(result) {
+      writeBin(jsonlite::base64_dec(result$data), "test.pdf")
+      remote$close()
+    }
+  )
+}
+
+Runtime$bindingCalled() %...>% {
+  print_pdf()
+}
+
+Runtime$enable() %...>% {
+  Page$enable()
+} %...>% {
+  Runtime$addBinding(name = "pagedownListener")
+} %...>% {
+  Page$navigate(url = "https://pagedown.rbind.io")
+} %...>% {
+  Runtime$evaluate(expression = "!!window.PagedPolyfill")
+} %...>% (
+  function(result) if(!isTRUE(result$result$value)) print_pdf()
+)
+
+
+# closing the session and chrome
+if(remote$is_alive) remote$close()
 rm(list = ls())
 gc()
