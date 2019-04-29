421a5068983879aa76cfb89cc95be21d94afc7e3
Romain Lesur <romain.lesur@gmail.com>
2019-04-29 23:49:34 +0200

    add a get_formals_for_event method in CDProtocol

--- R/CDProtocol.R
+++ R/CDProtocol.R
@@ -44,6 +44,24 @@ CDProtocol <- R6::R6Class(
     list_events = function(domain) {
       private$.list_objects(domain, "events")
     },
+    get_formals_for_event = function(domain, event) {
+      params_env <- private$.protocol$domains[[domain]]$events[[event]]$parameters
+      if(is.null(params_env)) {
+        params_names <- character(0)
+      } else {
+        params_names <- ls(params_env)
+      }
+      # since we will add a callback argument, check that callback is not already used:
+      stopifnot(!("callback" %in% params_names))
+      # here is the main difference with get_formals_for_command:
+      # in an event listener, all the parameters are optional
+      # we can directly add the callback argument...
+      params_names <- c(params_names, "callback")
+      # ... and return the list
+      res <- vector("list", length(params_names))
+      names(res) <- params_names
+      res
+    },
     list_types = function(domain) {
       private$.list_objects(domain, "types")
     },
--- tests/testthat/test-CDProtocol.R
+++ tests/testthat/test-CDProtocol.R
@@ -7,3 +7,9 @@ test_that("rlist2env transform nested list to nested env", {
                      c = rlang::env(rlang::empty_env(), d = 3))
   expect_equal(rlist2env(.l), .env)
 })
+
+test_that("get_formals_* methods return a list", {
+  protocol <- CDProtocol$new()
+  expect_is(protocol$get_formals_for_command("Page", "navigate"), "list")
+  expect_is(protocol$get_formals_for_event("Page", "loadEventFired"), "list")
+})
