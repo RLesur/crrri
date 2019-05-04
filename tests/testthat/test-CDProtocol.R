073fdb303086c7cc26e381954524451639ff7ce0
Christophe Dervieux <christophe.dervieux@gmail.com>
2019-05-04 17:19:35 +0200

    add test for get_formals_*

--- tests/testthat/test-CDProtocol.R
+++ tests/testthat/test-CDProtocol.R
@@ -28,3 +28,49 @@ test_that("get_formals_* methods return a list", {
   expect_is(protocol$get_formals_for_command("Page", "navigate"), "list")
   expect_is(protocol$get_formals_for_event("Page", "loadEventFired"), "list")
 })
+
+test_that("get_formals_* throws error if callback is present and returns correcly otherwise", {
+  protocol <- CDProtocol$new()
+  # create a dummy domain to add
+  dummyDomain <- rlang::env(
+    commands = rlang::env(
+      dummy_cmd = rlang::env(
+        parameters = rlang::env(
+          callback = list(name = "callback", description = "callback")
+        )),
+      dummy_cmd2 = rlang::env(
+        parameters = rlang::env(
+          dummy_param2 = list(name = "dummy_param_opt",
+                                 description = "dummy_desc option",
+                                 optional = TRUE),
+          dummy_param = list(name = "dummy_param", description = "dummy_dec")
+        ))
+    ),
+    events = rlang::env(
+      dummy_event = rlang::env(
+        parameters = rlang::env(
+          callback = list(name = "callback", description = "callback")
+        )
+      ),
+      dummy_event2 = rlang::env(
+        parameters = rlang::env(
+          dummy_param = list(name = "dummy_param", description = "dummy_desc"),
+          dummy_param2 = list(name = "dummy_param2",
+                                 description = "dummy_desc option")
+        )
+      )
+    ))
+  # this is hacky but works to test and not depend on the json protocol
+  rlang::env_bind(.env = protocol$.__enclos_env__$private$.protocol$domains,
+                  dummyDomain = dummyDomain)
+  expect_error(protocol$get_formals_for_command("dummyDomain", "dummy_cmd"), regexp = "callback")
+  expect_error(protocol$get_formals_for_event("dummyDomain", "dummy_event"), regexp = "callback")
+  expect_identical(
+    protocol$get_formals_for_event("dummyDomain", "dummy_event2"),
+    list(dummy_param = NULL, dummy_param2 = NULL, callback = NULL)
+  )
+  expect_identical(
+    protocol$get_formals_for_command("dummyDomain", "dummy_cmd2"),
+    list(dummy_param = rlang::sym(""), dummy_param2 = NULL, callback = NULL)
+  )
+})
