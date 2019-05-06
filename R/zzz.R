21bf3b76ca7e848252168a4c14e772fbe4ddf8e7
Christophe Dervieux <christophe.dervieux@gmail.com>
2019-05-06 19:51:10 +0200

    don't message in onLoad and exclude from coverage
    closes #48

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,7 @@
+## nocov start
+
 .onLoad <- function(libname, pkgname) {
-  if(requireNamespace("debugme", quietly = TRUE)) {
-    debugme::debugme()
-  } else {
-    if ("crrri" %in% strsplit(Sys.getenv("DEBUGME"), split = ",")[[1]]) {
-      packageStartupMessage("It seems you have this package in your DEBUGME variable,",
-              "but you have not install the debugme package yet.\n",
-              "You need to install it to the the log messages.")
-    }
-  }
+  if(requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
 }
+
+## nocov end
